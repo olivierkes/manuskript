cdcf184c10a31185a40e16aa7e2d30d0907b4a95
Curtis Gedak <gedakc@gmail.com>
2017-05-07 12:57:48 -0600

    Fixes: epiphany section in basic infos for characters not saved #43

--- manuskript/load_save/version_1.py
+++ manuskript/load_save/version_1.py
@@ -42,6 +42,7 @@ characterMap = OrderedDict([
     (Character.motivation, "Motivation"),
     (Character.goal, "Goal"),
     (Character.conflict, "Conflict"),
+    (Character.epiphany, "Epiphany"),
     (Character.summarySentence, "Phrase Summary"),
     (Character.summaryPara, "Paragraph Summary"),
     (Character.summaryFull, "Full Summary"),
