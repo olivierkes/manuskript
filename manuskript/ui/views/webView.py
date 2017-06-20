2beb3a939bc4db05289d8a76ad2e26b1f1c85a9e
Olivier Keshavjee <olivier.keshavjee@gmail.com>
2017-06-20 15:24:15 +0200

    Conditional import of QtWebEngine or QtWebKit

--- manuskript/ui/views/webView.py
+++ manuskript/ui/views/webView.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python
 # --!-- coding: utf8 --!--
-from PyQt5.QtWidgets import QTextEdit
+import PyQt5
+import os
 
-try:
+features = {'qtwebkit': False, 'qtwebengine': False}
+
+if 'QT_WEB' in os.environ:
+    features[os.environ['QT_WEB']] = True
+else:
+    try:
+        import PyQt5.QtWebKitWidgets
+        features['qtwebkit'] = True
+    except:
+        features['qtwebkit'] = False
+
+    try:
+        import PyQt5.QtWebEngineWidgets
+        features['qtwebengine'] = True
+    except:
+        features['qtwebengine'] = False
+
+if features['qtwebkit']:
     from PyQt5.QtWebKitWidgets import QWebView
     print("Debug: Web rendering engine used: QWebView")
     webEngine = "QtWebKit"
     webView = QWebView
-
-except:
-
-    try:
-        from PyQt5.QtWebEngineWidgets import QWebEngineView
-        print("Debug: Web rendering engine used: QWebEngineView")
-        webEngine = "QtWebEngine"
-        webView = QWebEngineView
-
-    except:
-        print("Debug: Web rendering engine used: QTextEdit")
-        webEngine = "QTextEdit"
-        webView = QTextEdit
+elif features['qtwebengine']:
+    from PyQt5 import QtWebEngineWidgets
+    print("Debug: Web rendering engine used: QWebEngineView")
+    webEngine = "QtWebEngine"
+    webView = QtWebEngineWidgets.QWebEngineView
+else:
+    from PyQt5.QtWidgets import QTextEdit
+    print("Debug: Web rendering engine used: QTextEdit")
+    webEngine = "QTextEdit"
+    webView = QTextEdit
