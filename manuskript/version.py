5d462d6a47fd631785fdb99755edd405988d5114
Curtis Gedak <gedakc@gmail.com>
2017-09-28 13:26:04 -0600

    Single source the package version
    Define one place to store the Manuskript version so that it can be
    used in multiple locations.
    
    Reference:
    
    Single-sourcing the package version
    https://packaging.python.org/guides/single-sourcing-package-version/

---
+++ manuskript/version.py
@@ -0,0 +1,9 @@
+# --!-- coding: utf8 --!--
+
+# Single source the package version
+# https://packaging.python.org/guides/single-sourcing-package-version/
+
+__version__ = "0.4.0"
+
+def getVersion():
+    return __version__
