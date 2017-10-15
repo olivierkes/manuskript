33f1a9b840f6193327cf4febdc8996622dd877bd
Olivier Keshavjee <olivier.keshavjee@gmail.com>
2017-10-15 14:53:05 +0200

    Adds icons to settings window

--- manuskript/ui/settings_ui.py
+++ manuskript/ui/settings_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'manuskript/ui/settings_ui.ui'
 #
-# Created: Sat Oct 14 16:47:16 2017
+# Created: Sun Oct 15 14:43:30 2017
 #      by: PyQt5 UI code generator 5.2.1
 #
 # WARNING! All changes made in this file will be lost!
