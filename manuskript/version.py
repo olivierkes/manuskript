701833ed7ef0ee1abcfefccd150842bd3f543b87
Olivier Keshavjee <olivier.keshavjee@gmail.com>
2017-10-31 21:25:27 +0100

    Bumps version number

--- manuskript/version.py
+++ manuskript/version.py
@@ -3,7 +3,7 @@
 # Single source the package version
 # https://packaging.python.org/guides/single-sourcing-package-version/
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 def getVersion():
     return __version__
