70e627409c26e7b9e7127b3b1d3b7ae2985f6d5f
Curtis Gedak <gedakc@gmail.com>
2018-04-06 11:15:45 -0600

    Enhance Travis CI build on OSX to remove sudo ownership warning
    The Travis CI build reports the following in the messages from
    executing package/prepare_osx.sh:
    
        sudo pip3 install --upgrade pip setuptools wheel
        The directory '/Users/travis/Library/Caches/pip/http' or its       \
         parent directory is not owned by the current user and the cache   \
         has been disabled. Please check the permissions and owner of that \
         directory. If executing pip with sudo, you may want sudo's -H     \
         flag.
        The directory '/Users/travis/Library/Caches/pip' or its parent     \
         directory is not owned by the current user and caching wheels has \
         been disabled. check the permissions and owner of that            \
         directory. If executing pip with sudo, you may want sudo's -H     \
         flag.
    
    In an effort to address this issue, add "-H" flag to sudo.

--- package/prepare_osx.sh
+++ package/prepare_osx.sh
@@ -4,7 +4,7 @@ brew update
 # upgrade to python 3.x
 brew upgrade python
 brew install enchant
-sudo pip3 install --upgrade pip setuptools wheel
+sudo -H pip3 install --upgrade pip setuptools wheel
 pip3 install pyinstaller PyQt5 lxml pyenchant
 brew install qt hunspell
 # fooling PyEnchant as described in the wiki: https://github.com/olivierkes/manuskript/wiki/Package-manuskript-for-OS-X
