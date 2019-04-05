3b34abdd4e924e881065a956a28c4f58d2fe225a
Temmon <tanilwynn@gmail.com>
2019-04-04 17:03:59 -0700

    Added ability to open the file with a comment in an editor when clicking on the table rows if necessary. Focus will also be set to that file.

--- manuskript/ui/tools/commentList.py
+++ manuskript/ui/tools/commentList.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # --!-- coding: utf8 --!--
+
 from PyQt5.QtCore import QVariant, Qt
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from PyQt5.QtWidgets import QWidget, QHeaderView
 
 from manuskript import settings
 from manuskript.ui.tools.comment_ui import Ui_CommentList
+from manuskript.models import references as Ref
+
 import re
 
 class commentList(QWidget, Ui_CommentList):
@@ -15,53 +18,93 @@ class commentList(QWidget, Ui_CommentList):
         self.mw = mainWindow
 
         self.btnFindComments.clicked.connect(self.findComments)
+        self.tblComments.clicked.connect(self.jumpToComment)
 
         self.tblComments.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
 
         #Populate the list as soon as you enter the screen
         self.findComments()
 
+    def jumpToComment(self, idx):
+        item = self.mdl.itemFromIndex(idx)
+        match = self.itemToMatch[id(item)]
+
+        index = self.mw.mdlOutline.indexFromItem(match.container)
+
+        if index.isValid():
+            self.mw.tabMain.setCurrentIndex(self.mw.TabRedac)
+            self.mw.mainEditor.setCurrentModelIndex(index, newTab=True)
+            return True
+        else:
+            print("Ref not found")
+            return False
+
+    def _findInText(item, breadcrumbs, isRoot=False):
+        txt = item.text()
+
+        #I don't like having root in the breadcrumbs
+        if not isRoot:
+            breadcrumbs.append(item.title())
+
+        #Find all comment blocks
+        matchIter = re.finditer(r"<!--(.*?)-->", txt)
+
+        lst = []
+        for m in matchIter:
+            match = commentList._shorten(m.group(0))
+            paraCount = commentList._countParagraphs(txt, m.start(0))
+
+            lst.append(_CommentMatch(item, match, breadcrumbs, paraCount))
+
+        #Recursive search through the children
+        for c in item.children():
+            lst += commentList._findInText(c, breadcrumbs[:])
+
+        return lst
+
+    def _countParagraphs(text, end):
+        #Humans typically index from 1
+        return text[:end].count("\n") + 1
+
+    def _shorten(note):
+        maxLen = 75
+
+        note = note[4:-3].strip()
+        if len(note) > maxLen:
+            note = note[:maxLen] + "..."
+        return note
+
 
     def findComments(self):
         root = self.mw.mdlOutline.rootItem
 
-        def shorten(note):
-            maxLen = 75
+        lst = commentList._findInText(root, [], True)
 
-            note = note.strip()
-            if len(note) > maxLen:
-                note = note[:maxLen] + "..."
-            return note
+        self.mdl = QStandardItemModel()
+        self.mdl.setHorizontalHeaderLabels([self.tr("Comment"), self.tr("File"), self.tr("Paragraph")])
 
-        def findInText(item, breadcrumbs):
-            txt = item.text()
-
-            #I don't like having root in the breadcrumbs
-            if item != root:
-                breadcrumbs.append(item.title())
-
-            #Find all comment blocks. Using a capture block so the tokens aren't in the find
-            lst = re.findall(r"<!--(.*?)-->", txt)
-
-            #Trim off the tokens
-            lst = [(shorten(t), breadcrumbs) for t in lst]
-
-            #Recursive search through the children
-            for c in item.children():
-                lst += findInText(c, breadcrumbs[:])
-
-            return lst
-
-        lst = findInText(root, [])
-
-        mdl = QStandardItemModel()
-        mdl.setHorizontalHeaderLabels([self.tr("Comment"), self.tr("File")])
+        self.itemToMatch = {}
 
         for t in lst:
-            mdl.appendRow([QStandardItem(t[0]), QStandardItem("/".join(t[1]))])
+            textItem = QStandardItem(t.text)
+            breadcrumbItem = QStandardItem("/".join(t.breadcrumbs))
+            paragraphItem = QStandardItem(str(t.paragraph))
 
-        self.tblComments.setModel(mdl)
+            self.itemToMatch[id(textItem)] = t
+            self.itemToMatch[id(breadcrumbItem)] = t
+            self.itemToMatch[id(paragraphItem)] = t
+
+            self.mdl.appendRow([textItem, breadcrumbItem, paragraphItem])
+
+        self.tblComments.setModel(self.mdl)
 
 
     def updateSettings(self):
         pass
+
+class _CommentMatch():
+    def __init__(self, container, text, breadcrumbs, paragraph):
+        self.container = container
+        self.text = text
+        self.breadcrumbs = breadcrumbs
+        self.paragraph = paragraph
