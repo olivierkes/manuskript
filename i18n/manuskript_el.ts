d7f03bd76c81705326945f501befc17c053e7851
J. Lavoie <j.lavoie@net-c.ca>
2020-07-22 15:43:08 +0000

    Translated using Weblate (Greek)
    Currently translated at 0.9% (8 of 861 strings)
    
    Translation: Manuskript/Translations
    Translate-URL: https://hosted.weblate.org/projects/manuskript/translations/el/

--- i18n/manuskript_el.ts
+++ i18n/manuskript_el.ts
@@ -504,7 +504,7 @@ Use that if you get YAML related error.</source>
     <message>
         <location filename="../manuskript/ui/mainWindow.ui" line="277"/>
         <source>Email</source>
-        <translation type="unfinished"></translation>
+        <translation>Διευθύνσεις ηλεκτ. ταχυδ.</translation>
     </message>
     <message>
         <location filename="../manuskript/ui/mainWindow.ui" line="1422"/>
