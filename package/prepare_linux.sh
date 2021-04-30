9bdd80f24420b9d33215d8d9cb309e490c5fb705
TheJackiMonster <thejackimonster@gmail.com>
2021-04-30 17:05:09 +0200

    Reverted mistakes in prepare_linux-CI script
    Signed-off-by: TheJackiMonster <thejackimonster@gmail.com>

--- package/prepare_linux.sh
+++ package/prepare_linux.sh
@@ -5,7 +5,7 @@ sudo apt-get -qq install python3-pip python3-dev \
      build-essential qt5-default libxml2-dev libxslt1-dev \
      mesa-utils libgl1-mesa-glx libgl1-mesa-dev
 
-#pyenv local 3.6.7
-python3 --version
-#easy_install pip
-pip install pyqt5 lxml pytest pytest-faulthandler
+pyenv local 3.6.7
+python --version
+easy_install pip
+pip install pyqt5==5.9 lxml pytest pytest-faulthandler
