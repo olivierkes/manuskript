3448e9e2c5f618ff6a75206ede1c0f4b2077afe2
TheJackiMonster <thejackimonster@gmail.com>
2021-11-21 21:32:12 +0100

    Adjusted the windows build script but it is still not working
    Signed-off-by: TheJackiMonster <thejackimonster@gmail.com>

--- package/build_for_windows.sh
+++ package/build_for_windows.sh
@@ -4,15 +4,24 @@ EXEC_DIR=$(pwd)
 DIR=$(mktemp -d /tmp/manuskript-windows.XXXXXX)
 PREFIX=$DIR/.wine
 
+BUILD_ARCH="win32"
+PY_VERSION="3.8.10"
+
+PY_NAME="python-$PY_VERSION"
+
+if [ "$BUILD_ARCH" = "win64" ]; then
+	PY_NAME="$PY_NAME-amd64"
+fi
+
 # Install Python:
-PY_DOWNLOAD="https://www.python.org/ftp/python/3.8.10/python-3.8.10.exe"
+PY_DOWNLOAD="https://www.python.org/ftp/python/$PY_VERSION/$PY_NAME.exe"
 PY_SETUP=$(echo $PY_DOWNLOAD | tr '/' ' ' | awk '{ print $(NF) }')
 
 if [ ! -e $PY_SETUP ]; then
 	wget $PY_DOWNLOAD
 fi
 
-WINEPREFIX=$PREFIX WINEARCH="win32" wine $PY_SETUP /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+WINEPREFIX=$PREFIX WINEARCH=$BUILD_ARCH wine $PY_SETUP /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 
 # Install Pandoc:
 PAN_DOWNLOAD="https://github.com/jgm/pandoc/releases/download/2.16.1/pandoc-2.16.1-windows-x86_64.msi"
@@ -22,13 +31,19 @@ if [ ! -e $PAN_SETUP ]; then
 	wget $PAN_DOWNLOAD
 fi
 
-WINEPREFIX=$PREFIX WINEARCH="win32" wine $PAN_SETUP /qn /norestart
+WINEPREFIX=$PREFIX WINEARCH=$BUILD_ARCH wine $PAN_SETUP /qn /norestart
+
+PY_DIR="Python$(echo $PY_VERSION | sed -e s/\\./\ /g - | awk '{ print $1$2 }')"
+
+if [ "$BUILD_ARCH" = "win32" ]; then
+	PY_DIR="$PY_DIR-32"
+fi
 
 # Install most dependencies with pip:
-cd $PREFIX/drive_c/Program\ Files/Python38-32/
+cd $PREFIX/drive_c/Program\ Files/$PY_DIR/
 
 pip_install() {
-	WINEPREFIX=$PREFIX WINEARCH="win32" wine python.exe Scripts/pip.exe install $@
+	WINEPREFIX=$PREFIX WINEARCH=$BUILD_ARCH wine python.exe Scripts/pip.exe install $@
 }
 
 # Upgrade pip to mitigate problems:
@@ -56,7 +71,7 @@ cd manuskript
 PKG_VERSION=$(grep -E "__version__.*\".*\"" "manuskript/version.py" | cut -d\" -f2)
 
 # Run PyInstaller to create the build:
-WINEPREFIX=$PREFIX WINEARCH="win32" wine pyinstaller manuskript.spec
+WINEPREFIX=$PREFIX WINEARCH=$BUILD_ARCH wine pyinstaller manuskript.spec
 cat build/manuskript/warn-manuskript.txt
 
 cd dist/manuskript
@@ -73,17 +88,32 @@ rm api-ms-win-*
 
 # Test Manuskript:
 ### comment: Seems to work fine...
-#WINEPREFIX=$PREFIX WINEARCH="win32" wine manuskript.exe
+WINEPREFIX=$PREFIX WINEARCH=$BUILD_ARCH wine manuskript.exe &
+WINE_TEST_PID=$!
 
-ZIP_NAME=manuskript-$PKG_VERSION-win32.zip
+sleep 5
+cd ..
+
+tmp_cleanup() {
+	# Cleanup everything:
+	### comment: removing the local git repository 
+	###          requires write permissions...
+	chmod +w -R $DIR/manuskript
+	rm -r $DIR
+}
+
+if [ $(ps $WINE_TEST_PID | grep manuskript.exe | wc -l) -gt 0 ]; then
+	kill $WINE_TEST_PID
+else
+	echo "ERROR: Package has crashed to an critical error!"
+	tmp_cleanup
+	exit
+fi
+
+ZIP_NAME=manuskript-$PKG_VERSION-$BUILD_ARCH.zip
 
 # Package everything together:
-cd ..
 zip -r $ZIP_NAME manuskript
 mv $ZIP_NAME $EXEC_DIR
 
-# Cleanup everything:
-### comment: removing the local git repository 
-###          requires write permissions...
-chmod +w -R $DIR/manuskript
-rm -r $DIR
+tmp_cleanup
