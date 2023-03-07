2ead298b00563fe74b0caea4335cf0f17676025a
tntscreed <tntscreed@proton.me>
2023-03-07 21:19:29 +0100

    Removed the absolute path left in by pyuic.
    Sorry about that. It didn't exactly doxx me, but it was weird.

--- manuskript/ui/bulkInfoManager.py
+++ manuskript/ui/bulkInfoManager.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'D:\OpensourceWork\manuskript\manuskript\ui\bulkInfoManager.ui'
+# Form implementation generated from reading ui file 'manuskript\ui\bulkInfoManager.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
