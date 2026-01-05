322499afeb2d536595967dde21302d4136c4e51a
KristofOfTheNorth <49472004+Kristof5933@users.noreply.github.com>
2026-01-05 15:30:26 +0100

    fix: the "exit" button in the 'about' dialog doesn't work

--- manuskript/ui/aboutDialog.py
+++ manuskript/ui/aboutDialog.py
@@ -2,9 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from manuskript.ui.abstractDialog import AbstractDialog
-
+from gi.repository import Gtk
 
 class AboutDialog(AbstractDialog):
 
     def __init__(self, mainWindow):
         AbstractDialog.__init__(self, mainWindow, "ui/about.glade", "about_dialog")
+
+    def initWindow(self, builder, window):
+        super().initWindow(builder, window)
+        self.window.connect("response", self.on_response)
+
+    def on_response(self, dialog, response_id):
+        if response_id == Gtk.ResponseType.DELETE_EVENT:
+            dialog.hide()
